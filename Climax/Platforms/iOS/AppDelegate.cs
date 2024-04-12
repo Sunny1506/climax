8f5843f78f4f2a12640837bb73da157e8931b1eb
MariaManu <emanuelebrumrossi0@gmail.com>
2024-04-12 08:18:52 -0300

    começo do app - XAML

---
+++ Climax/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Climax;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
